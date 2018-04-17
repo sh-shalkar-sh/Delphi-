19f3d6adacffaba1bdf56582615ffa72b85c893b
shalkar <sh-shalkar-sh@mail.ru>
2018-04-17 16:21:33 +0600

    Авторизация

---
+++ Auth/Unit2.pas
@@ -0,0 +1,27 @@
+unit Unit2;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Data.DB, Data.Win.ADODB;
+
+type
+  TDataModule2 = class(TDataModule)
+    ADOConnection1: TADOConnection;
+    ADOQuery1: TADOQuery;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  DataModule2: TDataModule2;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
