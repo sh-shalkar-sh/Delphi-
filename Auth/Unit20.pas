26eb8ca533c1232074a6b83e88fda515be2d9ae1
Mendygul <esdolda@gmail.com>
2018-04-18 12:09:26 +0600

    Added Form20

---
+++ Auth/Unit20.pas
@@ -0,0 +1,24 @@
+unit Unit20;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm20 = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form20: TForm20;
+
+implementation
+
+{$R *.dfm}
+
+end.
